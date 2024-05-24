1d249bf9952b3bb10fe21d56835a7b80bf4b1e1e
itsmesauravk <sauravkarki10.12@gmail.com>
2024-05-24 23:11:22 +0545

    added fetch on see user admin

--- admin/src/Pages/AdminSeeUsers.jsx
+++ admin/src/Pages/AdminSeeUsers.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AdminNavbar from '../Components/AdminNavbar'
 
 const AdminSeeUsers = () => {
+
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
+
+  const getAllUsers = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/admin/show-users`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+      })
+
+      const data = await response.json()
+      console.log(data)
+      if(data.success){
+        setUsers(data.users)
+      }
+    } catch (error) {
+      console.log(error)
+    }finally{
+      setLoading(false)
+    }
+
   return (
     <>
     <AdminNavbar />
@@ -10,4 +36,5 @@ const AdminSeeUsers = () => {
   )
 }
 
+}
 export default AdminSeeUsers
